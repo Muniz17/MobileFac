358875d77bbeab993f07a3a3bbf89f058049f9ff
Muniz17 <matheushenriquemuniz15@gmail.com>
2025-03-27 10:04:53 -0300

    Feito as Paginas do SQLite

---
+++ Carros/Models/Veiculo.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+
+namespace Carros.Models
+{
+    public class Veiculo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Modelo { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Fabricacao { get; set; }
+
+        public string Ano { get; set; }
+    }
+}
